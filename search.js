d5538802d8336e70d8d86c383f2f1a9f135477a6
Abdullah Ridwa <57331836+abdullahridwanJService@users.noreply.github.com>
2019-11-03 17:33:17 -0500

    Add files via upload

---
+++ search.js
@@ -0,0 +1,37 @@
+var btnSearch = document.getElementById("btnSearch");
+
+btnSearch.addEventListener('click', ()=>{
+    var e = document.getElementById("value");
+
+    
+    var value = e.options[e.selectedIndex].value;
+    var minDate = document.getElementById("minDate").value;
+    var maxDate = document.getElementById("maxDate").value;
+    console.log(value);
+    console.log(minDate);
+    const api = `https://jservice.io/api/clues?value=${value}&min_date=${minDate}&max_date=${maxDate}`
+    
+    fetch(api)
+    .then(response =>{
+        //convert info to json format
+        console.log(response);
+        return response.json();
+    })
+    .then(data=> {
+        console.log(data);
+        for(i=0 ; i<data.length ;  i++){
+            var tableValue = data[i].value;
+            var tableQuestion = data[i].question;
+            var tableAnswer = data[i].answer;
+            document.getElementById("tableBody").innerHTML += 
+            "<tr>"
+            + "<th scope=\"row\">" + (i+1) + "</th>"
+            + "<td>" + tableValue + "</td>"
+            + "<td>" + tableQuestion + "</td>"
+            + "<td>" + tableAnswer + "</td>"
+            + "</tr>"
+        }
+
+    })
+    
+})
